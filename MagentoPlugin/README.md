4e6cd5f9d4848e413cfb3326af5eeb3255dc6bec
Petar Borisovski <petar@customgento.com>
2021-02-18 08:22:24 +0100

    Delete extra directory

--- MagentoPlugin/README.md
+++
@@ -1,1 +0,0 @@
-# module-koality-m2
--- MagentoPlugin/registration.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Koality_MagentoPlugin', __DIR__);
