b246a8e78bf7d30a38496dc4fb54defae9a34fd7
Petar <petar@customgento.com>
2021-03-11 12:00:40 +0100

    Refactor

---
+++ Test/Integration/ConfigTest.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Koality\MagentoPlugin\Test\Integration;
+
+use Magento\TestFramework\Helper\Bootstrap;
+use PHPUnit\Framework\TestCase;
+use Koality\MagentoPlugin\Model\Config;
+
+class ConfigTest extends TestCase
+{
+    /**
+     * @var Config
+     */
+    private $config;
+
+    protected function setUp(): void
+    {
+        $objectManager           = Bootstrap::getObjectManager();
+        $this->config            = $objectManager->create(Config::class);
+    }
+
+    /**
+     * @magentoAppIsolation  enabled
+     * @magentoConfigFixture koality/rush_hour/rush_hour_begin 10:00
+     */
+    public function testRetrieveRushHourBegin(): void
+    {
+        self::assertEquals('10:10', $this->config->getRushHourBegin());
+    }
+
+    /**
+     * @magentoAppIsolation  enabled
+     * @magentoConfigFixture koality/rush_hour/rush_hour_end 17:00
+     */
+    public function testRetrieveRushHourEnd(): void
+    {
+        self::assertEquals('17:00', $this->config->getRushHourBegin());
+    }
+
+    /**
+     * @magentoAppIsolation  enabled
+     * @magentoConfigFixture koality/rush_hour/orders_per_hour_rushHour 50
+     */
+    public function testOrdersPerRushHour(): void
+    {
+        self::assertEquals(50, $this->config->getOrdersPerRushHour());
+    }
+
+    /**
+     * @magentoAppIsolation  enabled
+     * @magentoConfigFixture koality/rush_hour/include_weekends 1
+     */
+    public function testRushHourIncludedWeekend(): void
+    {
+        self::assertEquals(true, $this->config->doesRushHourHappenWeekends());
+    }
+
+    /**
+     * @magentoAppIsolation  enabled
+     * @magentoConfigFixture koality/opening_hours/orders_per_hour_normal 10
+     */
+    public function testOrdersPerHourNormal(): void
+    {
+        self::assertEquals(10, $this->config->getOrdersPerHourNormal());
+    }
+
+    /**
+     * @magentoAppIsolation  enabled
+     * @magentoConfigFixture koality/open_carts/open_carts 10
+     */
+    public function testOpenCartsExists(): void
+    {
+        self::assertEquals(10, $this->config->getOpenCarts());
+    }
+
+    /**
+     * @magentoAppIsolation  enabled
+     * @magentoConfigFixture koality/active_products/active_products 100
+     */
+    public function testGetActiveProducts(): void
+    {
+        self::assertEquals(100, $this->config->getActiveProducts());
+    }
+
+    /**
+     * @magentoAppIsolation  enabled
+     * @magentoConfigFixture koality/newsletter_subscribers/newsletter_subscriptions 100
+     */
+    public function testGetNewsletterSubscribers(): void
+    {
+        self::assertEquals(100, $this->config->getNewsletterSubscribers());
+    }
+}
+
